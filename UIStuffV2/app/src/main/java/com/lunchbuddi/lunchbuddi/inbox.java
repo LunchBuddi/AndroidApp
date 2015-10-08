fbaf9357982d36b016a43d973afdc29ff1349b75
Jack Brown <jack.brown.255@gmail.com>
2015-10-08 18:10:49 -0500

    Removed extra files, moved build.gradle to the right folder, and am working on typos. This is an early commit so that David can look at it.

--- UIStuffV2/app/src/main/java/com/lunchbuddi/lunchbuddi/inbox.java
+++ UIStuffV2/app/src/main/java/com/lunchbuddi/lunchbuddi/inbox.java
@@ -8,20 +8,20 @@ import java.util.List;
 
 // THIS CLASS IS ASSOCIATED WITH THE INBOX
 
-public class activity_inbox extends Activity {
+public class inbox extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inbox);
 
-        ListView listView = (ListView) findViewById(R.id.list);
+        ListView listView = (ListView) findViewById(R.id.listViewForInbox);
 
         // Create an empty array list of strings
-        List<String> items = new ArrayList<String>();
+        ArrayList<String> items = new ArrayList<String>();
 
         // Set the adapter
-        InboxAdapter adapter = new InboxAdapter(items);  //ERROR?
+        InboxAdapter adapter = new InboxAdapter(this, items);
         listView.setAdapter(adapter);
 
         // Set the emptyView to the ListView
