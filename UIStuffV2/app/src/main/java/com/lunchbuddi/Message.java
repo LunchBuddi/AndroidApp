cb3138127574b33fd70d600e74f24e67b23e6226
Emily Black <emily.2014@yahoo.com>
2016-03-20 12:35:05 -0500

    Started Message.java class

---
+++ UIStuffV2/app/src/main/java/com/lunchbuddi/Message.java
@@ -0,0 +1,26 @@
+package com.lunchbuddi;
+import java.util.*;
+
+/**
+ * Created by Emily on 3/20/2016.
+ */
+public class Message
+{
+    private String name;
+    private ArrayList<Person> students;
+    private String address;
+    private String state;
+
+    /**
+     * Default Constructor
+     */
+    public Message()
+    {
+        name = null;
+        students = null;
+        address = null;
+        state = null;
+    }
+
+    // Wait for David until I write the rest
+}
